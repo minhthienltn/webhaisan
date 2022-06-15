8431507ee9ccbacc38de35e927c25803a5918610
minhthienltn <107091588+minhthienltn@users.noreply.github.com>
2022-06-15 12:02:20 +0700

    Add files via upload

---
+++ dangkidangnhap.js
@@ -0,0 +1,38 @@
+function dangky(){
+    var thanhcong = true;
+    var tendangnhap = document.form.dangnhap.value;
+    var hovaten = document.form.hovaten.value;
+    var password = document.form.password.value;
+    var number = document.form.sodienthoai.value;
+    var diachinha = document.form.diachigiaohang.value;
+    if(tendangnhap.length<6 || tendangnhap.length>30){
+        thanhcong=false;
+        document.getElementById("tendangnhap").innerText="Tên đăng nhập phải lớn hơn 6 ký tự và nhỏ hơn 30 ký tự";
+    }
+    if(tendangnhap.length==0){
+        thanhcong=false;
+        document.getElementById("tendangnhap").innerText="Tên đăng nhập không được để trống";
+    }
+    if(password==""){
+        thanhcong=false;
+        document.getElementById("password").innerText="Mật khẩu không được để trống";
+    }
+    if(hovaten.length<6 || hovaten.length>30){
+        thanhcong=false;
+        document.getElementById("hovaten").innerText="Họ và tên phải lớn hơn 6 ký tự và nhỏ hơn 30 ký tự";
+    }
+    if(isNaN(number)==true || number==""){
+        thanhcong=false;
+        document.getElementById("number").innerText="Số điện thoại phải ghi bằng số";
+    }
+    if(number==""){
+        thanhcong=false;
+        document.getElementById("number").innerText="Số điện thoại không được để trống";
+    }
+    if(diachinha==""){
+        thanhcong=false;
+        document.getElementById("diachigiaohang").innerText="Địa chỉ giao hàng không được để trống";
+    }
+    
+    return thanhcong;
+}
